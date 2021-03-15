4d26d6bbe69c6060c44ae4b2de1baf4376ad76d5
Munsch <73666974+MunschSam@users.noreply.github.com>
2021-03-15 13:35:20 +0100

    Add files via upload

---
+++ 1erquetephp.php
@@ -0,0 +1,11 @@
+<?php $nom = "Indiana Jones and the last crusade";
+$vue =true;
+$sortie= 1989;
+$note=4.5; ?>
+
+<ul>
+<li>titre:<?php echo $nom ?></li>
+<li>vue:<?php echo $vue ?></li>
+<li>année:<?php echo $sortie ?></li>
+<li>note:<?php echo $note ?></li>
+</ul> 
